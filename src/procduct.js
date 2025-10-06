b2e51e39fbe1288bbf488955a7a0d83f1ff1c588
Kingmax12 <oyedeletope559@gmail.com>
2025-10-06 10:40:37 +0100

    Update procduct.js

---
+++ src/procduct.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h2>My Products</h2>
+      <p>
+      Welcome here are some of My products
+        <li>https://683762eab670aee5ab6695d4--lambent-syrniki-000713.netlify.app/</li>
+        <li>https://cybertrucklandingpage.vercel.app/</li>
+      </p>
+    </div>
+  );
+};
+
+export default Products;
