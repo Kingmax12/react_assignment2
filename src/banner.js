a491e79b909c71a09b27bb3a97679fc0851fe8f2
Kingmax12 <oyedeletope559@gmail.com>
2025-10-06 10:43:37 +0100

    Update banner.js

---
+++ src/banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <h1>Hello! And  welcome to My Basic Site😊</h1>
+      <p>My name is Oyedele Temitope , a junior web-developer.
+    I am skilled in some aspect of software for example Html, css, Javascript etc. </p>
+    </div>
+  );
+};
+
+export default Banner;
